df1c93e4edd10d69d4fd1b0604fb53c5c9a34d03
Pavel <pavel.seduhin@list.ru>
2024-11-30 20:27:08 +0200

    first commit

---
+++ DZ_1/Zadanie_1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(void){
+    printf("\n Hello world!\n");
+    return 0;
+}
---
+++ DZ_1/Zadanie_3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main (void){
+    printf("   *\n");
+    printf("  ***\n");
+    printf(" *****\n");
+    printf("*******\n");
+    printf(" HH HH\n");
+    printf(" ZZZZZ\n");
+    return 0;
+}
